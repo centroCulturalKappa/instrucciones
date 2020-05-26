d77acbf3333d7c05c09a428e7801e39f6ffae16d
centroCulturalKappa <anarkxpop@gmail.com>
2020-05-26 11:23:21 -0500

    clear

--- clear/jstexto.js
+++ clear/jstexto.js
@@ -1,8 +1,13 @@
 var aceptar = function(){
   var counter = 25;
+  var toggler = 0;
+  var colores = ["black","white"];
+  console.log(document.getElementsByClassName("midText")[0]);
   var instervalo = setInterval(function(){
+      document.getElementsByClassName("midText")[0].style.backgroundColor = colores[toggler];
       document.getElementById("texto").style.lineHeight = counter+"px";
       counter -= 0.3;
+      toggler = (toggler+1)%2;
       if (counter<=0) {
         clearInterval(instervalo);
       }
