185ef1ee5ce7f933ae040e060de457ad95f29379
centroCulturalKappa <anarkxpop@gmail.com>
2020-05-26 11:45:50 -0500

    acl

---
+++ aclaraciones/jstexto.js
@@ -0,0 +1,15 @@
+var aceptar = function(){
+  var counter = 25;
+  var toggler = 0;
+  var colores = ["black","white"];
+  document.getElementById("botonerino").style.display = "none";
+  var instervalo = setInterval(function(){
+      document.getElementsByClassName("midText")[0].style.backgroundColor = colores[toggler];
+      document.getElementById("texto").style.lineHeight = counter+"px";
+      counter -= 0.3;
+      toggler = (toggler+1)%2;
+      if (counter<=0) {
+        clearInterval(instervalo);
+      }
+  },10);
+};
